2ce6bcce21026fb39d4da8cd4202edce5229d3dd
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-05-19 23:40:47 +0530

    [feat] : Student Model, [func]: Add student by Prof

--- portal/prof/views.py
+++ portal/prof/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from .models import Student
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Prof main site")
+    return render(request, 'prof/index.html')
+
+def add_student(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        student = Student(username=username, password=password)
+        student.save()
+        
+    return render(request, 'prof/add.html',{
+        'student_db': Student.objects.all()
+    })
