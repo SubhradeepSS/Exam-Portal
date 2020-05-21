fb0e9ca31adc34f426550867b4accc31de520958
Waryserpant122 <yashshah123boy@gmail.com>
2020-05-21 18:16:57 +0530

    [fix]: add in makepaper

--- portal/prof/views.py
+++ portal/prof/views.py
@@ -32,33 +32,46 @@ def add_question(request):
     return render(request, 'prof/question.html',{
         'question_db': Question_DB.objects.all()
     })
+
+c=[]
 def make_paper(request) :
     if request.method=='POST' :
         add_question_in_paper(request)
+        
     return render(request, 'prof/qpaper.html',{
         'qpaper_db' : Question_Paper.objects.all()
     } ) 
-
+c=[]
 def add_question_in_paper(request) :
+    d=[]
+    for i in Question_DB.objects.all() :
+        d.append(i)
     if request.method =='POST'  and request.POST.get('qpaper', False) != False :
         paper_title =request.POST['qpaper']
         question_paper=Question_Paper(qPaperTitle=paper_title)
         question_paper.save()
+        while c != [] :
+            c.pop()
         return render(request,'prof/addquestopaper.html' , {
             'qpaper' : question_paper ,
             'question_list' : Question_DB.objects.all()
         })
-    elif request.method == 'POST' :
+    elif request.method == 'POST' and request.POST.get('title', False) != False : 
         addques = request.POST['title']
         a = Question_DB.objects.get(qno=addques)
         title = request.POST['papertitle']
         b = Question_Paper.objects.get(qPaperTitle=title)
         b.questions.add(a)
+        # if a in d :
+        #     d.remove(a)
+        c.append(a)
         return render(request,'prof/addquestopaper.html' , {
             'qpaper' : b ,
-            'question_list' : Question_DB.objects.all()
+            'question_list' : d,
+            'elist' : c
         })
-        
+    while c != [] :
+        c.pop()
     return render(request,'prof/addquestopaper.html' )
 
 def view_paper(request) :
