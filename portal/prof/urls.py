b95e9f26bb52b0b4c0eea91811c0ee5cce4a0694
Waryserpant122 <yashshah123boy@gmail.com>
2020-05-21 18:28:23 +0530

    [fix]: Routing in QuetsionPaper

--- portal/prof/urls.py
+++ portal/prof/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.add_student, name='add_student'),
     path('question', views.add_question, name='add_question'),
-    path('makeqpaper', views.make_paper ,name='make_paper') ,
-    path('addquestion',views.add_question_in_paper, name="add_question_in_paper"),
-    path('viewpaper',views.view_paper,name='view_paper')
+    path('questionpaper', views.make_paper ,name='make_paper') ,
+    path('questionpaper/addquestion',views.add_question_in_paper, name="add_question_in_paper"),
+    path('questionpaper/viewpaper',views.view_paper,name='view_paper')
 ]
