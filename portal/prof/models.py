c88901ba65cf42ca7a16342936f549a6054f25b3
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-06-06 01:50:00 +0530

    [fix]: url name

--- portal/prof/models.py
+++ portal/prof/models.py
@@ -30,6 +30,7 @@ class QNO(models.Model):
     nid = models.AutoField(primary_key=True)
     def __str__(self):
         return f'{self.number}'
+        
 class Question_Paper(models.Model):
     
     qPaperTitle = models.CharField(max_length=100) 
