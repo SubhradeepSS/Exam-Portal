2ce6b984902899ffc0f28050db73909c4f6812ee
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-08-23 23:20:21 +0530

    Fixed stud url parameter type bug, Created groups and users

--- stud/urls.py
+++ stud/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'stud'
 
 urlpatterns = [
-    path('<int:stud_username>', views.index, name='index'),
-    path('<int:stud_username>/exams', views.view_exams, name='view_exams'),
-    path('<int:stud_username>/exams/<int:exam_id>', views.view_exam, name='view_exam'),
+    path('<str:stud_username>', views.index, name='index'),
+    path('<str:stud_username>/exams', views.view_exams, name='view_exams'),
+    path('<str:stud_username>/exams/<int:exam_id>', views.view_exam, name='view_exam'),
 ]
