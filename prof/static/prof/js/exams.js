ffa5ee278114378c63f15010362e475fb1ac2a71
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-11-01 12:20:09 +0530

    [refactor]: static code formatted

--- prof/static/prof/js/exams.js
+++ prof/static/prof/js/exams.js
@@ -1,23 +1,29 @@
 window.smoothScroll = function(target) {
     var scrollContainer = target;
-    do { //find scroll container
+    do { 
         scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
+        if (!scrollContainer) 
+            return;
+
+        scrollContainer.scrollTop++;
     } while (scrollContainer.scrollTop == 0);
 
     var targetY = 0;
-    do { //find the top of target relatively to the container
-        if (target == scrollContainer) break;
+    do { 
+        if (target == scrollContainer)
+            break;
+
         targetY += target.offsetTop;
     } while (target = target.offsetParent);
 
-    scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
+    scroll = (c, a, b, i) => {
+        i++; 
+        if (i > 30) 
+            return;
         c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+        setTimeout(() => { scroll(c, a, b, i); }, 20);
     }
-    // start scrolling
+    
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
 
